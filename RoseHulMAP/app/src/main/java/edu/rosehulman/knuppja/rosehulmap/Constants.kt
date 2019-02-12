6aa954da745d2d37023d9d2275bd21c6d2df1933
uenos <uenos@rose-hulman.edu>
2019-02-12 18:39:06 -0500

    Added code for logging in with email

---
+++ RoseHulMAP/app/src/main/java/edu/rosehulman/knuppja/rosehulmap/Constants.kt
@@ -0,0 +1,7 @@
+package edu.rosehulman.knuppja.rosehulmap
+
+object Constants {
+    const val TAG = "PIC"
+    const val PICS_COLLECTION = "Pics"
+    const val DEBUG = "Debug"
+}
--- RoseHulMAP/app/src/main/java/edu/rosehulman/knuppja/rosehulmap/MainActivity.kt
+++ RoseHulMAP/app/src/main/java/edu/rosehulman/knuppja/rosehulmap/MainActivity.kt
@@ -13,22 +13,27 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import android.content.ActivityNotFoundException
+import android.util.Log
+import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentReference
 
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
+class MainActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener,
+    SplashFragment.OnLoginButtonPressedListener{
 
     private lateinit var settingsRef: DocumentReference
     val auth = FirebaseAuth.getInstance()
     lateinit var authListener: FirebaseAuth.AuthStateListener
     private val RC_SIGN_IN = 1
     lateinit var uid: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
+        initializeListeners()
 
 
         val toggle = ActionBarDrawerToggle(
@@ -58,10 +63,15 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                // TODO: Sign out.
+                auth.signOut()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -128,5 +138,64 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    // TODO: add (and remove) an auth state listener upon start (and stop).
+    override fun onStart() {
+        super.onStart()
+        auth.addAuthStateListener(authListener)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        auth.removeAuthStateListener(authListener)
+    }
+
+    private fun initializeListeners() {
+        // TODO: Create an AuthStateListener that passes the UID
+        // to the MovieQuoteFragment if the user is logged in
+        // and goes back to the Splash fragment otherwise.
+        // See https://firebase.google.com/docs/auth/users#the_user_lifecycle
+        authListener = FirebaseAuth.AuthStateListener { auth ->
+            val user = auth.currentUser
+            Log.d(Constants.TAG, "In auth listener, User: $user")
+            if (user != null) {
+                this.uid = user.uid
+                Log.d(Constants.TAG, "Main UID: ${user.uid}")
+                Log.d(Constants.TAG, "Name: ${user.displayName}")
+                Log.d(Constants.TAG, "Email: ${user.email}")
+                Log.d(Constants.TAG, "Photo: ${user.photoUrl}")
+                Log.d(Constants.TAG, "Phone: ${user.phoneNumber}")
+                //switchToPicListFragment(user.uid)
+            } else {
+                switchToSplashFragment()
+            }
+        }
+    }
+
+    private fun switchToSplashFragment() {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, SplashFragment())
+        ft.commit()
+    }
+
+    override fun onLoginButtonPressed() {
+        launchLoginUI()
+    }
+
+    private fun launchLoginUI() {
+        // TODO: Build a login intent and startActivityForResult(intent, ...)
+        // For details, see https://firebase.google.com/docs/auth/android/firebaseui#sign_in
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+        val loginIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setLogo(R.drawable.ic_launcher_custom)
+            .build()
+
+        startActivityForResult(loginIntent, RC_SIGN_IN)
     }
 }
